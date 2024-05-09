c2b401e062f9f8b3ccd8dcebd688d7a1fc9c9dfb
Arturo Suárez <manuel.suarez@cimat.mx>
2024-05-09 11:11:01 -0600

    Configure directories

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ from rasterio.plot import show
 from matplotlib import pyplot as plt
 from PIL import Image
 
-
+work_dir = os.path.expanduser("~")
+data_dir = os.path.join(work_dir, "data", "dataset-cimat")
+tiff_dir = os.path.join(data_dir, "tiff")
+norm_dir = os.path.join(data_dir, "norm")
+var_dir = os.path.join(data_dir, "var_jpg")
+wind_dir = os.path.join(data_dir, "windfield_tiff")
+mask_dir = os.path.join(data_dir, "binary_mask_gimp_png")
