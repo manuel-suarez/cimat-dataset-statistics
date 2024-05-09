4bc7735e84497e0ce8b4051eff41dbe3fe3e5562
Arturo Suárez <manuel.suarez@cimat.mx>
2024-05-09 11:48:59 -0600

    Fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from rasterio.plot import show
 from matplotlib import pyplot as plt
 from PIL import Image
 
-work_dir = os.path.expanduser(".")
+work_dir = os.path.expanduser("~")
 data_dir = os.path.join(work_dir, "data", "dataset-cimat")
 tiff_dir = os.path.join(data_dir, "tiff")
 norm_dir = os.path.join(data_dir, "norm")
